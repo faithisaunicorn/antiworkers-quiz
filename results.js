81784ca2b2921bd3f00bdb80f550d10eeee02cbe
Faith <faithchoo126@hotmail.com>
2025-06-20 16:32:58 +0800

    Create results.js

---
+++ results.js
@@ -0,0 +1,74 @@
+window.ANTIWORKER_RESULTS = [
+  {
+    title: "Yuni Kon",
+    character: "Yuni",
+    img: "images/yuni.png",
+    mbti: ["INFJ"]
+  },
+  {
+    title: "Kerry Onn",
+    character: "Kerry",
+    img: "images/kerry.png",
+    mbti: ["INTJ"]
+  },
+  {
+    title: "Dr Phrog, PhD",
+    character: "Phrog",
+    img: "images/phrog.png",
+    mbti: ["INTP"]
+  },
+  {
+    title: "Schrodinger",
+    character: "Schrodinger",
+    img: "images/schrodinger.png",
+    mbti: ["INFP"]
+  },
+  {
+    title: "Tang Ping Win",
+    character: "Ping Win",
+    img: "images/pingwin.png",
+    mbti: ["ISTP", "ISFP"]
+  },
+  {
+    title: "Lao Hong",
+    character: "Lao Hong",
+    img: "images/laohong.png",
+    mbti: ["ISTJ", "ISFJ"]
+  },
+  {
+    title: "Fernando Greenfield",
+    character: "Fernando",
+    img: "images/fernando.png",
+    mbti: ["ESFJ"]
+  },
+  {
+    title: "Shane Finnerton",
+    character: "Shane",
+    img: "images/shane.png",
+    mbti: ["ESFP", "ESTP"]
+  },
+  {
+    title: "Henry Huffington III",
+    character: "Henry",
+    img: "images/henry.png",
+    mbti: ["ESTJ"]
+  },
+  {
+    title: "Basil Lee",
+    character: "Basil",
+    img: "images/basil.png",
+    mbti: ["ENFJ"]
+  },
+  {
+    title: "Mr Moneypenne",
+    character: "Moneypenne",
+    img: "images/moneypenne.png",
+    mbti: ["ENTJ"]
+  },
+  {
+    title: "Barkley Goldstein",
+    character: "Barkley",
+    img: "images/barkley.png",
+    mbti: ["ENFP", "ENTP"]
+  }
+];
